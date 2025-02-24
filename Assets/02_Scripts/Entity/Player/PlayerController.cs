cd6359c1cc78c254f29f2d08c765dd4208334ded
Hwang-Inseop <inseop.h02@gmail.com>
2025-02-24 20:48:38 +0900

    [Fix] 점프, 슬라이드 버그 수정
    점프, 더블점프 동시 작동 오류 수정
    슬라이드 작동 안되는 오류 수정

--- Assets/02_Scripts/Entity/Player/PlayerController.cs
+++ Assets/02_Scripts/Entity/Player/PlayerController.cs
@@ -1,11 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR;
 
-//플레이어가 가질 수 있는 상태 정의: Idle, 땅에 붙어있는 상태, 점프한 상태
-public enum PlayerState {idle, Grounded, isJumping, isSliding}
+//플레이어가 가질 수 있는 상태 정의: 땅에서 달리는 상태, 땅에 붙어있는 상태, 점프한 상태
+public enum PlayerState {isRunning, isJumping, isSliding}
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class PlayerController : MonoBehaviour
     public float maxJumpHoldTime = 0f; // 점프 키 누르는 시간의 한계 시간
     public float doubleJumpForce = 0f; // 더블 점프 힘
     private float jumpHoldTimer = 0f; // 점프 키 누르는 시간
+    private bool isRunning = false;
     private bool canDoubleJump = false;
     private bool isJumping = false;
     public bool isSliding = false;
@@ -24,10 +26,10 @@ public class PlayerController : MonoBehaviour
     
     private PlayerState playerState;
     
-    //게임 시작 즉시 Awake에서 상태를 Idle로
+    //게임 시작 즉시 Awake에서 상태를 isRunning로
     private void Awake()
     {
-        ChangeState(PlayerState.idle);
+        ChangeState(PlayerState.isRunning);
     }
 
     void Start()
@@ -40,12 +42,16 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z)) ChangeState(PlayerState.isJumping);
-        if (Input.GetKeyDown(KeyCode.X)) ChangeState(PlayerState.isSliding);
-
+        HandleInput();
         UpdateState();
     }
 
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Z)) ChangeState(PlayerState.isJumping);
+        if (Input.GetKeyDown(KeyCode.X)) ChangeState(PlayerState.isSliding);
+    }
+    
     private void UpdateState()
     {
         switch (playerState)
@@ -63,15 +69,16 @@ public class PlayerController : MonoBehaviour
     // 점프 조작 메서드
     void PlayerJump()
     {
-        if (Input.GetKeyDown(KeyCode.Z)) // Z키 처음 눌렀을 때
+        if (Input.GetKeyDown(KeyCode.Z) && isRunning) // Z키 처음 눌렀을 때
         {
+            isRunning = false;
             isJumping = true;
             canDoubleJump = true;
             jumpHoldTimer = 0f;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             Debug.Log("Jump");
         }
-        else if (canDoubleJump) // 공중에서 한 번 더 점프 가능
+        else if (Input.GetKeyDown(KeyCode.Z) && canDoubleJump) // 공중에서 한 번 더 점프 가능
         {
             canDoubleJump = false;
             rb.velocity = new Vector2(rb.velocity.x, doubleJumpForce);
@@ -98,14 +105,14 @@ public class PlayerController : MonoBehaviour
     #region 슬라이드 조작
     void PlayerSlide()
     {
-        if (!isSliding)
+        if (Input.GetKeyDown(KeyCode.X) && !isSliding && !isJumping)
         {
             isSliding = true;
             normalCollider.enabled = false; // 기본 콜라이더 비활성화
             slideCollider.enabled = true; // 슬라이딩용 콜라이더 활성화
             Debug.Log("Start Slide");
         }
-        else if (isSliding)
+        else if (Input.GetKeyUp(KeyCode.X) && isSliding)
         {
             isSliding = false;
             normalCollider.enabled = true; // 기본 콜라이더 활성화
@@ -117,6 +124,14 @@ public class PlayerController : MonoBehaviour
 
     private void ChangeState(PlayerState newState)
     {
+        if (newState == playerState) return;
         playerState = newState;
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+            isRunning = true;
     }
 }
