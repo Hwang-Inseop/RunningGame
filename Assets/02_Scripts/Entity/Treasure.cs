b7294964f05651c5e02d96731647f95bac837cd0
THGM1 <jennypsh@naver.com>
2025-02-24 12:30:32 +0900

    [FEAT] 보물 클래스 추가

--- Assets/02_Scripts/Entity/Treasure.cs
+++ Assets/02_Scripts/Entity/Treasure.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class Treasure : MonoBehaviour
 {
-    private bool isEquip = false;
+    public bool IsEquipped { get; private set; } // 장착 여부
+    [SerializeField] private string name; // 이름
+    [SerializeField] private string description; // 설명
+    [Header("보물 스탯")]
+    [SerializeField] private float speed; // 속도 증가량
+    [SerializeField] private float healthDrain; // 체력 감소량
+    [SerializeField] private bool canRevive; // 부활 가능 여부
 
     public void Equip()
     {
-        if (!isEquip)
+        if (!IsEquipped)
         {
-            isEquip = true;
+            IsEquipped = true;
+            // 플레이어 참조해서 추가 스탯 적용
+            if (canRevive)
+            {
+                // 낙사 했을 때 체력 받아와서 부활
+            }
+        }
+    }
 
+    public void Unequip()
+    {
+        if (IsEquipped)
+        {
+            IsEquipped = false;
+            // Equip() 로직 반대로
         }
     }
 }
+
