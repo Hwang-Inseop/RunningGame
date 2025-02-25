41a3f67596f1925c2a8788ba57b31101564de190
THGM1 <jennypsh@naver.com>
2025-02-25 11:01:01 +0900

    [Fix] 보물 스크립트 수정

--- Assets/02_Scripts/Entity/Treasure/CoinTreasure.cs
+++ Assets/02_Scripts/Entity/Treasure/CoinTreasure.cs
@@ -7,10 +7,26 @@ public class CoinTreasure : Treasure, ICoinGenerator
     [SerializeField] private GameObject extraCoin; // 추가 코인 생성
     [SerializeField] private Transform coinPosition; // 추가 코인 생성 위치
 
+    private void Start()
+    {
+        if (IsEquipped)
+        {
+            GenerateCoin();
+        }
+    }
     public void GenerateCoin()
     {
         if (extraCoin != null && coinPosition != null)
         {
+            StartCoroutine(SpawnCoin());
+        }
+    }
+
+    private IEnumerator SpawnCoin()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(intervalTime);
             Instantiate(extraCoin, coinPosition.position, Quaternion.identity);
         }
     }
