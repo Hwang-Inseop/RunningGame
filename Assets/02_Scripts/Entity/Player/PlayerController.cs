5471e55405d156631f27d08ebfb7c9840ebbcf18
Hwang-Inseop <inseop.h02@gmail.com>
2025-02-25 15:56:13 +0900

    [Feat] 플레이어 무적 상태 추가
    장애물과 트리거 충돌 시 잠시 무적 상태가 됨

--- Assets/02_Scripts/Entity/Player/PlayerController.cs
+++ Assets/02_Scripts/Entity/Player/PlayerController.cs
@@ -10,27 +10,34 @@ public enum PlayerState {isRunning, isJumping, isSliding}
 
 public class PlayerController : MonoBehaviour
 {
+    private bool isRunning = false; // idle 바닥에서 달리는 중인 상태
+    
     public float jumpForce = 0f; // 기본 점프 힘
     public float maxJumpHoldTime = 0f; // 점프 키 누르는 시간의 한계 시간
     public float doubleJumpForce = 0f; // 더블 점프 힘
     private float jumpHoldTimer = 0f; // 점프 키 누르는 시간
-    private bool isRunning = false;
-    private bool canDoubleJump = false;
-    private bool isJumping = false;
-    public bool isSliding = false;
+    private bool canDoubleJump = false; // 더블 점프 가능한 상태
+    private bool isJumping = false; //점프중인 상태
     
+    public bool isSliding = false; // 슬라이딩중인 상태
     public Collider2D normalCollider; // 기본 상태 콜라이더
     public Collider2D slideCollider; // 슬라이딩용 콜라이더
+    
+    public int maxHP; // 최대 HP
+    public int currentHP; // 현재 HP
+    public int damage; // 대미지 수치
+    public float hpDrainInterval = 1f; // 체력 지속 소모 시간 간격 (1초)
+    private bool damaged = false; // 대미지 입은 상태, true 되면 체력 감소, 잠시간 무적화
+    public float invincible; //무적 시간
     
     private Rigidbody2D rb;
     private Animator animator;
     
     private PlayerState playerState;
     
-    //게임 시작 즉시 Awake에서 상태를 isRunning로
     private void Awake()
     {
-        ChangeState(PlayerState.isRunning);
+        ChangeState(PlayerState.isRunning); //게임 시작 즉시 Awake에서 상태를 isRunning로
     }
 
     void Start()
@@ -38,8 +45,10 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         
-        // 기본적으로 슬라이딩 콜라이더 비활성화
-        slideCollider.enabled = false;
+        slideCollider.enabled = false; // 기본적으로 슬라이딩 콜라이더 비활성화
+        
+        currentHP = maxHP; // 게임 시작시 HP MAX
+        StartCoroutine(DrainHp()); // 체력 지속 소모 시작
     }
     
     void Update()
@@ -48,14 +57,18 @@ public class PlayerController : MonoBehaviour
         UpdateState();
     }
 
-    // 키 입력을 받으면 상태 전이
+    /// <summary>
+    /// 키 입력을 받으면 상태 전이 실행
+    /// </summary>
     void HandleInput()
     {
         if (Input.GetKeyDown(KeyCode.Z)) ChangeState(PlayerState.isJumping);
         if (Input.GetKeyDown(KeyCode.X)) ChangeState(PlayerState.isSliding);
     }
     
-    // 상태 전이 시 조작 메서드 실행
+    /// <summary>
+    /// 상태 전이되면 조작 메서드를 실행
+    /// </summary>
     private void UpdateState()
     {
         switch (playerState)
@@ -131,19 +144,75 @@ public class PlayerController : MonoBehaviour
     }
     #endregion
     
-    // 땅에서 달리는 상태
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Ground"))
+        if(collision.gameObject.CompareTag("Ground")) // Ground 태그 오브젝트와 닿아있으면 
             isRunning = true;
         animator.SetBool("isRunning", true);
         animator.SetBool("isJumping", false);
     }
 
+    /// <summary>
+    /// 상태 전이
+    /// </summary>
+    /// <param name="newState"></param>
     private void ChangeState(PlayerState newState)
     {
         if (newState == playerState) return;
         playerState = newState;
+    }
+    
+    /// <summary>
+    /// 달리는동안 체력 지속 소모
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator DrainHp()
+    {
+        while (currentHP > 0)
+        {
+            yield return new WaitForSeconds(hpDrainInterval);
+            TakeDamage(damage);
+            Debug.Log("HP: " + currentHP);
+        }
+    }
+    
+    // 장애물과 충돌하면 대미지, 잠시 무적
+    private void OnCollisionTrigger2D(Collider2D collision)
+    {
+        if(!damaged && collision.gameObject.CompareTag("Obstacle"))
+            TakeDamage(10);
+        StartCoroutine(Invincible());
+    }
+
+    IEnumerator Invincible()
+    {
+        damaged = true;
+        float invincibleTime = invincible;
         
+        yield return new WaitForSeconds(invincibleTime);
+        
+        damaged = false;
+    }
+    
+    /// <summary>
+    /// 대미지 처리하는 메서드
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeDamage(int damage)
+    {
+        if (!damaged)
+        {
+            currentHP -= damage;
+
+            if (currentHP <= 0)
+            {
+                Die();
+            }
+        }
+    }
+    
+    void Die()
+    {
+        Debug.Log("Die");
     }
 }
