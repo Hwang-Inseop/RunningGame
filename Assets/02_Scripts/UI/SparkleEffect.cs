484de701e5519de9ef85c8c8874f88708db22544
윤동영 <ehddud9608@naver.com>
2025-02-26 15:24:37 +0900

    [Feat] StartScene UI 애니메이션 추가
    StartScene UI 애니메이션 추가

---
+++ Assets/02_Scripts/UI/SparkleEffect.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SparkleEffect : MonoBehaviour
+{
+    public GameObject[] targetObject;
+    void Start()
+    {
+        foreach(GameObject stars in targetObject)
+        {
+            SpriteRenderer sr = stars.GetComponent<SpriteRenderer>();
+            if(sr!= null)
+            {
+                Color originalColor = sr.color;  //현재 컬러를 저장
+                Color yellowColor = Color.yellow;
+                Color brightColor = new Color(
+                    Mathf.Min(originalColor.r * 2.5f, 1f),
+                    Mathf.Min(originalColor.g * 2.5f, 1f),
+                    Mathf.Min(originalColor.b * 2.5f, 1f),
+                    originalColor.a);
+
+                Sequence seq = DOTween.Sequence(); //여러개의 애니메이션을 연속적으로 연결할 수 있는 Class
+                seq.Append(sr.DOColor(brightColor,1f)); //1초동안 brightColor설정값으로 변경
+                seq.Append(sr.DOColor(yellowColor, 1f));
+                seq.Append(sr.DOColor(originalColor, 1f));
+                sr.DOFade(0.3f, 1f)
+                    .SetLoops(-1, LoopType.Yoyo)
+                    .SetEase(Ease.InOutSine);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/02_Scripts/UI/StartDisplay.cs
+++ Assets/02_Scripts/UI/StartDisplay.cs
@@ -1,5 +1,7 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StartDisplay : MonoBehaviour
@@ -15,30 +17,73 @@ public class StartDisplay : MonoBehaviour
     public Vector2 maxPosition;
 
     [Header("애니메이션 설정")]
-    public float animationDuration = 3f; // 애니메이션 지속 시간
-    public float spawnOffset = 2f;
+    public float animationDuration; // 애니메이션 지속 시간
+    public float spawnOffset;
+
+    [Header("캐릭터 간의 간격")]
+    public float horizontal;
+
+    private List<GameObject> spawnedPlayers = new List<GameObject>(); //두번째 애니메이션을 위한 위치 저장 리스트
     void Start()
     {
+
+        Vector3 objectPosition = transform.position;
+        Vector2 relativeMin = new Vector2(objectPosition.x + minPosition.x, objectPosition.y + minPosition.y);
+        Vector2 relativeMax = new Vector2(objectPosition.x + maxPosition.x, objectPosition.y + maxPosition.y);
         if(characterSprites.Length<countPlayer)
         {
             Debug.LogError("캐릭터 생성시 에러 발생");
         }
         for(int i=0; i<countPlayer; i++)
         {
-            float randX = Random.Range(minPosition.x, maxPosition.x);
-            float randY = Random.Range(minPosition.y, maxPosition.y);
+            float randX = Random.Range(relativeMin.x, relativeMax.x) + horizontal;
+            float randY = Random.Range(relativeMin.y, relativeMax.y);
             Vector3 targetPosition = new Vector3(randX, randY, 0); //랜덤 위치로 생성
 
             Vector3 spawnPosition = targetPosition + new Vector3(0, spawnOffset, 0);
 
             GameObject displayPlayer = new GameObject($"{i}번째 DisplayPlayer");
-        }
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+            SpriteRenderer sr = displayPlayer.AddComponent<SpriteRenderer>();
+            if (i < characterSprites.Length) //sprite 할당
+            {
+                sr.sprite = characterSprites[i];
+            }
+            else
+            {
+                sr.sprite = characterSprites[0]; //예외처리
+                Debug.LogError("캐릭터 이미지가 없습니다.");
+            }
+            displayPlayer.transform.position = spawnPosition; //초기 위치
+            displayPlayer.transform.localScale = Vector3.one * 1.5f; //초기 스케일보다 1.5배 크도록
+
+            spawnedPlayers.Add(displayPlayer); //리스트에 추가
+
+            StartCoroutine(AnimateEntry(displayPlayer, targetPosition));
+        }
+    }
+    IEnumerator AnimateEntry(GameObject player, Vector3 targetPosition)
     {
-        
+        float timer = 0f;
+        Vector3 startPosition = player.transform.position;
+
+        while (timer < animationDuration)
+        {
+            timer += Time.deltaTime;
+            float moveSpeed = timer / animationDuration; //애니메이션이 작동되는 동안의 스피드
+
+            player.transform.position = Vector3.Lerp(startPosition, targetPosition, moveSpeed); //애니메이션의 위치
+
+            yield return null;
+        }
+        player.transform.position = targetPosition;
+    }
+    public void OnStartButtonClicked()
+    {
+        float secondDuration = 2f;
+        foreach(GameObject player in spawnedPlayers)
+        {
+            player.transform.DOMoveY(-10f, secondDuration).SetEase(Ease.InQuad);
+        }
     }
 }
