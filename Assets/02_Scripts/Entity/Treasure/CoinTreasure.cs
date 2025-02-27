9c8c35e0a0d157c1f53d2b394f5993dbde36ddbf
THGM1 <jennypsh@naver.com>
2025-02-27 11:36:46 +0900

    [Fix] 코인 생성 보물 수정
    - 생성 후 플레이어에게 끌려오도록 수정

--- Assets/02_Scripts/Entity/Treasure/CoinTreasure.cs
+++ Assets/02_Scripts/Entity/Treasure/CoinTreasure.cs
@@ -1,3 +1,4 @@
+using RunningGame.Entity;
 using RunningGame.Managers;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,27 +11,49 @@ public class CoinTreasure : Treasure
     private bool hasSpawned = false;
     private void Start()
     {
-        coinPosition.position = transform.position;
-        coinPosition.position = transform.position + new Vector3(5, 0, 0);
+        GameObject playerSpawnPos = GameObject.Find("Grid");
+        coinPosition = playerSpawnPos.transform;
+        //coinPosition.position += new Vector3(5, 0,0);   
+        coinPosition.SetParent(playerSpawnPos.transform);
     }
     public override void ApplyEffect(Player player)
     {
         if (extraCoin != null &&  !hasSpawned)
         {
-            StartCoroutine(GenerateCoin());
+            StartCoroutine(GenerateCoin(player));
         }
     }
 
-    private IEnumerator GenerateCoin()
+    private IEnumerator GenerateCoin(Player player)
     {
-        Debug.Log("코루틴");
         hasSpawned = true;
         while (true)
         {
             yield return new WaitForSeconds(intervalTime);
-            Instantiate(extraCoin, coinPosition.position, Quaternion.identity);
+            GameObject newCoin = Instantiate(extraCoin, coinPosition.position, Quaternion.identity);
+            newCoin.transform.SetParent(coinPosition);
             Debug.Log("코인 생성");
+            StartCoroutine(Magnetic(newCoin.transform, player.transform));
 
+        }
+    }
+    public IEnumerator Magnetic(Transform coin, Transform player)
+    {
+        Debug.Log("코루틴");
+        while (coin != null)
+        {
+            float duration = 1f; // 이동하는 데 걸리는 시간
+            float elapsedTime = 0f;
+
+            Vector3 startPos = coin.position;
+            Vector3 targetPos = player.position;
+
+            while (elapsedTime < duration && player != null)
+            {
+                coin.position = Vector3.Lerp(startPos, targetPos, elapsedTime / duration);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
         }
     }
 }
